04f79be372aba0f08126a386f81beb1b4473dbda
svn <svn@51023cdb-f5e0-e311-a53e-4c72b9b181c9>
2014-06-04 20:52:57 +0000

    Completed 1-8
    git-svn-id: svn+ssh://svn.pricetx.org/home/svn/tcpl@10 51023cdb-f5e0-e311-a53e-4c72b9b181c9

---
+++ chapter1/1-8/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int c, bl, ta, nl;
+
+	bl = 0;
+	ta = 0;
+	nl = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ')
+			++bl;
+		else if (c == '\t')
+			++ta;
+		else if (c == '\n')
+			++nl;
+	}
+	printf("Blanks: %d | Tabs: %d | Newlines %d\n", bl, ta, nl);
+}
